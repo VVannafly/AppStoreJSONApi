040fd027cf9367a1223b3b7721bb2ef0238e177a
Dmitry <wodi220500@gmail.com>
2021-02-09 19:47:38 +0300

    fix: some refactoring search screen

---
+++ AppStoreJSONApi/Helpers/VerticalStackView.swift
@@ -0,0 +1,23 @@
+//
+//  VerticalStackView.swift
+//  AppStoreJSONApi
+//
+//  Created by Dmitriy Chernov on 09.02.2021.
+//
+
+import UIKit
+
+class VerticalStackView: UIStackView {
+
+    init(arrangedSubviews: [UIView], spacing: CGFloat = 0) {
+        super.init(frame: .zero)
+        arrangedSubviews.forEach({addArrangedSubview($0)})
+        self.spacing = spacing
+        self.axis = .vertical
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
--- AppStoreJSONApi/Views/SearchResultCell.swift
+++ AppStoreJSONApi/Views/SearchResultCell.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class SearchResultCell: UICollectionViewCell {
     
-    let imageView: UIImageView = {
+    let appIconImageView: UIImageView = {
         let iv = UIImageView()
         iv.backgroundColor = .systemRed
         iv.widthAnchor.constraint(equalToConstant: 64).isActive = true
@@ -47,34 +47,49 @@ class SearchResultCell: UICollectionViewCell {
         return button
     }()
     
+    lazy var screenShot1ImageView = self.createScreenShotImageView()
+    lazy var screenShot2ImageView = self.createScreenShotImageView()
+    lazy var screenShot3ImageView = self.createScreenShotImageView()
+    
+    func createScreenShotImageView() -> UIImageView {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .systemBlue
+        return imageView
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-//        backgroundColor = .systemGreen
-        let labelsStackView = UIStackView(arrangedSubviews: [
-            nameLabel, categoryLabel, ratingsLabel
-        ])
-        labelsStackView.axis = .vertical
-        
-        let stackView = UIStackView(arrangedSubviews: [
-            imageView,
-            labelsStackView,
-            getButton
-        ])
-        stackView.spacing = 12
-        stackView.alignment = .center
-        addSubview(stackView)
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(equalTo: self.topAnchor),
-            stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10),
-            stackView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10),
-            stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
-        ])
+        //        backgroundColor = .systemGreen
+        configureViews()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    private func configureViews() {
+        let labelsStackView = VerticalStackView(arrangedSubviews: [
+            nameLabel, categoryLabel, ratingsLabel
+        ])
+        
+        let infoTopStackView = UIStackView(arrangedSubviews: [
+            appIconImageView,
+            labelsStackView,
+            getButton
+        ])
+        infoTopStackView.spacing = 12
+        infoTopStackView.alignment = .center
+        
+        let screenshotStackView = UIStackView(arrangedSubviews: [
+            screenShot1ImageView, screenShot2ImageView, screenShot3ImageView
+        ])
+        screenshotStackView.spacing = 12
+        screenshotStackView.distribution = .fillEqually
+        
+        let overallStackView = VerticalStackView(arrangedSubviews: [infoTopStackView, screenshotStackView], spacing: 16)
+        addSubview(overallStackView)
+        overallStackView.fillSuperview(padding: .init(top: 16, left: 16, bottom: 16, right: 16))
+        
+    }
     
 }
