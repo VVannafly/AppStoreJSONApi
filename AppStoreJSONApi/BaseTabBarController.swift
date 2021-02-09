524869ad677ee367f13a1355b4c2fc29d3cf585f
Dmitry <wodi220500@gmail.com>
2021-02-09 15:00:56 +0300

    feat: Nav controller

---
+++ AppStoreJSONApi/BaseTabBarController.swift
@@ -0,0 +1,35 @@
+//
+//  BaseTabBarController.swift
+//  AppStoreJSONApi
+//
+//  Created by Dmitriy Chernov on 09.02.2021.
+//
+
+import UIKit
+
+class BaseTabBarController: UITabBarController {
+    
+    // 2 - refacotr our repeated logic inside of vieDidLoad
+    // 3 - maybe introduce our AppSearchController
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+ 
+        viewControllers = [
+            createNavController(viewController: UIViewController(), title: "Today", imageName: "today_icon"),
+            createNavController(viewController: UIViewController(), title: "Apps", imageName: "apps"),
+            createNavController(viewController: UIViewController(), title: "Search", imageName: "search")
+        ]
+    }
+    
+    fileprivate func createNavController(viewController: UIViewController, title: String, imageName: String) -> UIViewController {
+    let navController = UINavigationController(rootViewController: viewController)
+        navController.navigationBar.prefersLargeTitles = true
+        viewController.navigationItem.title = title
+        viewController.view.backgroundColor = .systemBackground
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = UIImage(named: imageName)
+        return navController
+    }
+}
